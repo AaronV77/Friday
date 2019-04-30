3431ac64f3f66685a4d1cef53e1e0cc0bba9c5c3
AaronV77 <aaronvaloroso77@gmail.com>
2019-04-29 21:00:15 -0500

    Added some memory options to my version one.

--- Version-1/libc/mem.h
+++ Version-1/libc/mem.h
@@ -2,9 +2,13 @@
 #define KHEAP_H
 
 #include "../include/stdint.h"
+#include "../drivers/screen.h"
 
-void memory_copy(uint8_t *source, uint8_t *destination, int bytes);
 void memset(int *dest, int val, int len);
-// int kmalloc(int sz, int align, int *phys);
+void memory_copy(unsigned char *source, unsigned char *destination, int bytes);
+void * malloc();
+void heap_install();
+int find_memory_hole();
+void free(int * pointer);
 
 #endif
